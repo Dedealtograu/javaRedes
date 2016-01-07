c0cf716c5ddf3a00a49a83acc0560ceeb02050dd
Dedealtograu <matias.jms14@gmail.com>
2016-01-07 09:39:39 -0200

    Browser

---
+++ src/lerArquivos/ReadServeFileTest.java
@@ -0,0 +1,12 @@
+package lerArquivos;
+
+import javax.swing.JFrame;
+
+public class ReadServeFileTest {
+
+	public static void main(String[] args) {
+		ReadServeFile application = new ReadServeFile();
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
